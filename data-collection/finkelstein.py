89848681d6468e744ffdeb1720047b9177f8e75d
Michael Edward <michael.anzuoni@cielo24.com>
2013-02-21 14:29:54 -0500

    Modularized

---
+++ data-collection/finkelstein.py
@@ -0,0 +1,46 @@
+import csv
+import os
+import sys
+
+mentat_path = os.path.abspath('../mentat/')
+sys.path.append(mentat_path)
+from graph_search import get_similarity_score as get_scores
+
+
+input_file = sys.argv[1]
+
+
+def _read_in_csv(input_file):
+    with open(input_file, 'rb') as f:
+        fink_csv = csv.reader(f)
+        node1 = []
+        node2 = []
+        scores = []
+        for row in fink_csv:
+            node1.append(row[0])
+            node2.append(row[1])
+            scores.append(row[2])
+    return (node1, node2, scores)
+
+
+def _generate_cnet_scores(comparisons):
+    node1 = comparisons[0]
+    node2 = comparisons[1]
+    fink_scores = comparisons[2]
+    for index in range(len(fink_scores)):
+        print node1[index]
+        try:
+            print get_scores(node1[index], node2[index])
+        except:
+            print -1
+
+
+def generate_data(input_fle=input_file):
+    fink_tuple = _read_in_csv(input_file)
+    _generate_cnet_scores(fink_tuple)
+
+
+if __name__ == '__main__':
+    generate_data()
+
+
---
+++ mentat/ConceptNetNode.py
@@ -0,0 +1,77 @@
+from __future__ import unicode_literals
+import urllib2
+import json
+
+
+class ConceptNetNode(object):
+    '''
+    This is a wrapper object for Conceptnet DB results that makes it easier to work with in terms of graph search. 
+    The following are fields from the JSON result that are then converted in Python object attributes by way of
+    self.create_attrs_from_response()
+    
+    id: the unique ID for this edge, which contains a SHA-1 hash of the information that makes it unique.
+    uri: the URI of the assertion being expressed. The uri is not necessarily unique, because many edges can bundle together to express the same assertion.
+    rel: the URI of the predicate (relation) of this assertion.
+    start: the URI of the first argument of the assertion.
+    end: the URI of the second argument of the assertion.
+    weight: the strength with which this edge expresses this assertion. A typical weight is 1, but weights can be higher, lower, or even negative.
+    sources: the sources that, when combined, say that this assertion should be true (or not true, if the weight is negative).
+    license: a URI representing the Creative Commons license that governs this data. See Copying and sharing ConceptNet.
+    dataset: a URI representing the dataset, or the batch of data from a particular source that created this edge.
+    context: the URI of the context in which this statement is said to be true.
+    features: a list of three identifiers for features, which are essentially assertions with one of their three components missing.
+    surfaceText: the original natural language text that expressed this statement. '''
+
+    def __init__(self, node_name, request=None, convert=False):
+        self.node_name = node_name
+        self.cnet_edges = {}
+        if request != None:
+            self.cnet_response = self.search_solr(request)
+        else:
+            self.cnet_response = self.search_solr(self.build_request(self.node_name))
+        if self.cnet_response != {}:
+            self.edges = self.create_adjacency_list(self.cnet_response)
+            self.create_attrs_from_response(self.cnet_response)
+        if convert:
+            self.convert_list_to_nodes()
+
+    def create_attrs_from_response(self, response):
+        try:
+            for key, value in response['response']['docs'][0].iteritems():
+                setattr(self, key, value)
+        except:
+            pass
+
+    def build_request(self, node_name, rows=100, format="json"):
+        formatted_name = node_name.replace('_', '+')
+        formatted_name = '"' + formatted_name + '"'
+        request = u"http://localhost:8983/solr/select/?q=startLemmaString:{0}&version=2.2&start=0&rows={1}&indent=on&wt={2}".format(formatted_name, rows, format)
+        return request
+
+    def create_adjacency_list(self, cnet_response):
+        adjacent_nodes = []
+        edges = {}
+        for entry in cnet_response['response']['docs']:
+            adjacent_nodes.append(entry['endLemmaString'])
+        lemma = self.node_name.replace('_', ' ')
+        edges[lemma] = adjacent_nodes
+        return edges
+
+    def convert_list_to_nodes(self):
+        for key, value in self.edges.iteritems():
+            for lemma in value:
+                search_term = lemma.replace(' ', '_')
+                value = ConceptNetNode(search_term)
+                self.cnet_edges[lemma] = value
+
+    def search_solr(self, request_string):
+        req = urllib2.Request(request_string)
+        opener = urllib2.build_opener()
+        json_result = {}
+        try:
+            f = opener.open(req)
+            json_result = json.loads(f.read())
+        except UnicodeEncodeError:
+            pass
+
+        return json_result
